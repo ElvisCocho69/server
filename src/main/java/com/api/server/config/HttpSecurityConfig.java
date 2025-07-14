5fe77f0a80a927a508e9da652bfbbb094df18ed0
Sergio Avila <savilar@outlook.es>
2025-07-14 00:41:19 -0500

    app properties

--- src/main/java/com/api/server/config/HttpSecurityConfig.java
+++ src/main/java/com/api/server/config/HttpSecurityConfig.java
@@ -68,7 +68,7 @@ public class HttpSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://ip-172-16-0-248.ec2.internal", "http://13.218.102.160", "http://fammeba.duckdns.org", "fammeba.duckdns.org", "www.fammeba.duckdns.org", "http://3.235.249.63"));
+        configuration.setAllowedOrigins(Arrays.asList( "http://ip-172-16-0-248.ec2.internal", "http://fammeba.duckdns.org", "http://3.235.249.63"));
 
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
